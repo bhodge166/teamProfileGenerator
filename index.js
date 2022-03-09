2a6e454769a60bae6b22f3acf799d423f84d6921
Brad Hodge <bhodge166@gmail.com>
2022-03-08 17:17:24 -0700

    added function to classes to generate cards. changed html markup. added base function to create html

--- index.js
+++ index.js
@@ -4,3 +4,118 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const generateHtml = require("./src/createHtml");
+const team = [];
+
+const managerQuestions = [
+  {
+    message: "What is the managers name?",
+    name: "name",
+    type: "input",
+  },
+  {
+    message: "What is the managers id?",
+    name: "id",
+    type: "input",
+  },
+  {
+    message: "What is the managers email",
+    name: "email",
+    type: "input",
+  },
+  {
+    message: "What is the managers office number?",
+    name: "officeNumber",
+    type: "input",
+  },
+];
+
+const employeeQuestions = [
+  {
+    message: "What type of employee would you like to add?",
+    name: "role",
+    type: "list",
+    choices: ["Engineer", "Intern"],
+  },
+  {
+    message: (answers) => `What is the name of the ${answers.role}?`,
+    name: "name",
+    type: "input",
+  },
+  {
+    message: (answers) => `What is the id of the ${answers.role}?`,
+    name: "id",
+    type: "input",
+  },
+  {
+    message: (answers) => `What is the email of the ${answers.role}?`,
+    name: "email",
+    type: "input",
+  },
+  {
+    message: (answers) => {
+      if (answers.role === "Engineer") {
+        return `What is the github username of the ${answers.role}?`;
+      } else {
+        return `What school did the ${answers.role} go to?`;
+      }
+    },
+    name: "extra",
+    type: "input",
+  },
+];
+
+function main() {
+  inquirer.prompt(managerQuestions).then((data) => {
+    const manager = new Manager(
+      data.name,
+      data.id,
+      data.email,
+      data.officeNumber
+    );
+    team.push(manager);
+    addEmployee();
+  });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt({
+      message: "What would you like to do?",
+      name: "choice",
+      type: "list",
+      choices: ["Add an employee", "Create webpage"],
+    })
+    .then((data) => {
+      if (data.choice === "Add an employee") {
+        inquirer.prompt(employeeQuestions).then((data) => {
+          if ((data.role = "Engineer")) {
+            const employee = new Engineer(
+              data.name,
+              data.id,
+              data.email,
+              data.extra
+            );
+            team.push(employee);
+          } else {
+            const employee = new Intern(
+              data.name,
+              data.id,
+              data.email,
+              data.extra
+            );
+            team.push(employee);
+          }
+          addEmployee();
+        });
+      } else {
+        createHtml();
+      }
+    });
+}
+
+function createHtml() {
+  console.log("Hello");
+}
+
+main();
--- src/createHtml.js
+++ src/createHtml.js
@@ -1,4 +1,9 @@
-function generateHtml(data) {
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+const Manager = require("../lib/Manager");
+
+function generateHtml(team) {
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,16 +11,38 @@ function generateHtml(data) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <title>My team</title>
 </head>
 <body>
-<header></header>
+<h1 class= "bg-danger text-align-center justify-content-center>My team</h1>
 <main class = "container">
     <section class = "row">
-        <div></div>
+    ${team
+      .map((team) =>
+        team.generateCard(team.officeNumber || team.github || team.school)
+      )
+      .join("\n")}
     </section>
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>   
 </body>
 </html>`;
 }
+
+module.exports = generateHtml;
+<div class="col-sm-6">
+  <div class="card" style="width: 18rem;">
+    <div class="card-body bg-primary text-light">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+    </ul>
+  </div>
+</div>;
